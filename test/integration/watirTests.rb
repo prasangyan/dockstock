970ef132ca502a144317ce5bbb31c99f8f6e7131
Prashant Angyan <prashant@itxsolutionsindia.com>
2011-11-05 19:00:48 +0530

    Added a min-height for the content area

--- test/integration/watirTests.rb
+++ test/integration/watirTests.rb
@@ -26,6 +26,7 @@ site = "versavault.com"
 testEmail = "prashant@blendit.com.my"
 testEmailPassword = "password@123"
 signUpURL = "http://versavault.com/register"
+name = "Prashant Angyan"
 loginEmailIdField = "emailid"
 browser.goto site
 
@@ -94,9 +95,10 @@ messageToShowAfterRegistration = '<h2>You are all set, <name>. Thanks!</h2>
 <p>You can start uploading files easily. Just download the <a href="<download link>">Versa Vault Desktop Client</a>
 and you can start storing and sharing documents. We also sent you an email with all the details on how to get back here</p>'
 
-browser.text_field(:id,"emailid").set testEmail
-browser.text_field(:id,"password").set testEmailPassword
-browser.text_field(:id,"confirm_password").set testEmailPassword
+browser.text_field(:id => "name").set name
+browser.text_field(:id => "emailid").set testEmail
+browser.text_field(:id => "password").set testEmailPassword
+browser.text_field(:id => "confirm_password").set testEmailPassword
 browser.link(:id,"BtnSignUp").click
 
 testResult = browser.url == "http://versavault.com/dashboard"
