6e37d1cfc49fdd22394f66e37b5d166ba2ab2380
Santhosh <santhoshonet@gmail.com>
2011-12-17 16:22:35 +0530

    removed index for session

--- db/schema.rb
+++ db/schema.rb
@@ -94,7 +94,7 @@ ActiveRecord::Schema.define(:version => 20111214114345) do
   end
 
   #add_index "user_sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "user_sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+  #add_index "user_sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "shared_s3_objects", :force => true do |t|
     t.integer  "s3_object_id"
