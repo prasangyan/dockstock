334b0a756f2bff8b0541c70967070d87b08f30dc
Santhosh <santhoshonet@gmail.com>
2011-12-17 16:17:30 +0530

    removed index from schema

--- db/schema.rb
+++ db/schema.rb
@@ -93,7 +93,7 @@ ActiveRecord::Schema.define(:version => 20111214114345) do
     t.datetime "updated_at"
   end
 
-  add_index "user_sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  #add_index "user_sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "user_sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "shared_s3_objects", :force => true do |t|
